0ee385aa7dad99f3bd0a64142253f957e08a4dd1
Dawid Górny <hello@dawidgorny.com>
2019-01-11 00:29:12 +0100

    Added initial dropdown template

--- src/views/main.js
+++ src/views/main.js
@@ -1,11 +1,13 @@
 import html from 'choo/html';
 import LoadingOverlay from '../components/loading-overlay';
 import Map from '../components/map';
+import Dropdown from '../components/dropdown';
 
 export default function mainView (state, emit) {
   return html`
     <div class="relative db w-100 h-100">
       ${state.cache(Map, 'map').render()}
+      ${state.cache(Dropdown, 'destination').render()}
       ${state.cache(LoadingOverlay, 'loading-overlay').render()}
     </div>
   `;
