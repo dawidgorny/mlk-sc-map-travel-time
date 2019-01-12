44c32784bbb27927180c632b1c74c8af3b9b720a
Dawid Górny <hello@dawidgorny.com>
2019-01-12 21:51:20 +0100

    Added mode-switch component

---
+++ src/components/mode-switch/index.js
@@ -0,0 +1,62 @@
+import html from 'choo/html';
+import raw from 'choo/html/raw';
+import Component from 'choo/component';
+import style from './style.css';
+import merge from '../../utils/merge';
+import nanostate from 'nanostate';
+
+import TramBlueImage from './tram-blue.svg';
+import CarBlueImage from './car-blue.svg';
+
+export default class ModeSwitch extends Component {
+  constructor (id, state, emit) {
+    super(id, state, emit);
+    this.id = id;
+    this.state = state;
+    this.emit = emit;
+    this.isDirty = false;
+    this.local = state.components[id] = merge([{
+      value: 'transit'
+    }, state.components && state.components[id] ? state.components[id] : {}]);
+    this.setState();
+    style.use();
+  }
+
+  setState () {
+    
+  }
+
+  load (element) {
+    
+  }
+
+  update () {
+    let dirty = this.isDirty | false;
+    
+    if (dirty) {
+      this.setState();
+    }
+    return dirty;
+  }
+
+  createElement () {
+    const s = this.local;
+
+    const onClick = (e) => {
+      const value = e.currentTarget.getAttribute('data-toggle-value');
+      s.value = value;
+      this.isDirty = true;
+      this.emit('render');
+      this.emit(`${this.id}:valueChange`, value);
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    };
+
+    return html`
+    <div class="fl w-30 ph5 ${style.locals['mode-switch']}">
+      <a data-toggle-value="transit" class="ph1 ${s.value === 'transit' ? style.locals['active'] : ''}" href="#" onclick=${onClick}><img src="${TramBlueImage}" width="60" height="29"></a>
+      <a data-toggle-value="driving" class="ph1 ${s.value === 'driving' ? style.locals['active'] : ''}" href="#" onclick=${onClick}><img src="${CarBlueImage}" width="60" height="29"></a>
+    </div>`;
+  }
+}
--- src/views/main.js
+++ src/views/main.js
@@ -3,10 +3,12 @@ import LoadingOverlay from '../components/loading-overlay';
 import Map from '../components/map';
 import Dropdown from '../components/dropdown';
 import AddressSearch from '../components/address-search';
+import ModeSwitch from '../components/mode-switch';
 
 export default function mainView (state, emit) {
   return html`
     <div class="relative db w-100 h-100">
+      ${state.cache(ModeSwitch, 'mode-switch').render()}
       ${state.cache(Dropdown, 'destination').render()}
       ${state.cache(AddressSearch, 'address-search').render()}
       ${state.cache(Map, 'map').render()}
