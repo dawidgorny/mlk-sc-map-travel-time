534d03e47bb7fc2cc5f1968e49b4d8cda2ef270e
Dawid Górny <hello@dawidgorny.com>
2019-01-13 23:36:46 +0100

    Changed main layout

--- src/views/main.js
+++ src/views/main.js
@@ -8,12 +8,12 @@ import LoadingOverlay from '../components/loading-overlay';
 
 export default function mainView (state, emit) {
   return html`
-    <div class="relative db w-100 h-100">
+    <div class="db w-100 h-100">
       ${state.cache(ModeSwitch, 'mode-switch').render()}
       ${state.cache(Dropdown, 'destination').render()}
-      ${state.cache(AddressSearch, 'address-search').render()}
-      <div class="relative db w-100 h-100">
+      <div class="relative dib w-100 h-100">
         ${state.cache(Map, 'map').render()}
+        ${state.cache(AddressSearch, 'address-search').render()}
         ${this.state.cache(Tooltip, 'tooltip').render()}
         ${this.state.cache(LoadingOverlay, 'loading-overlay').render()}
       </div>
